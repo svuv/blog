a394455f0d796fc09be94a6865f345996cc9fb46
svuv <1737748525@qq.com>
2023-03-04 18:58:19 +0800

    项目搭建

---
+++ config/conf_system.go
@@ -0,0 +1,9 @@
+package config
+
+type Logger struct {
+	Level        string `yaml:"Level"`
+	Prefix       string `yaml:"prefix"`
+	Director     string `yaml:"director"`
+	ShowLine     bool   `yaml:"show_line"`
+	LogInConsole bool   `yaml:"log_in_console"`
+}
